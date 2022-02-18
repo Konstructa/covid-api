72865139aa418043259e75ea3a14431254c8cafd
Konstructa <elenprojects7@gmail.com>
2022-02-17 21:17:34 -0300

    adicionando russia-china e alterando nome

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,10 +7,37 @@ import { Resultado } from './interface/result.interface';
 export class AppService {
   constructor(private httpService: HttpService) {}
 
-  async getHello() {
+  async getUsaAndBrazil() {
     const result: Resultado[] = [];
 
     const url = 'https://disease.sh/v3/covid-19/countries/brazil,usa';
+
+    for (let i = 0; i < 2; i++) {
+      const data = this.httpService.get(url).pipe(
+        map((res) => res.data[i]),
+        map(
+          (data): Resultado => ({
+            país: data.country,
+            casosHoje: data.todayCases,
+            mortesHoje: data.todayDeaths,
+            data: new Date(),
+            ativos: data.active,
+            estadoCritico: data.critical,
+          }),
+        ),
+      );
+
+      const observable = await lastValueFrom(data);
+      result.push(observable);
+    }
+
+    return result;
+  }
+
+  async getChinaAndRussia() {
+    const result: Resultado[] = [];
+
+    const url = 'https://disease.sh/v3/covid-19/countries/china,russia';
 
     for (let i = 0; i < 2; i++) {
       const data = this.httpService.get(url).pipe(
