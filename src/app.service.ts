965f4c94a84365ace9376c50317cf95039f7742d
Konstructa <elenprojects7@gmail.com>
2022-02-22 16:51:04 -0300

    criando função unica que pega os dados

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,42 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom, map } from 'rxjs';
 import { Resultado } from './interface/result.interface';
+import { parse } from 'json2csv';
 
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
 
-  async getUsaAndBrazil() {
-    const result: Resultado[] = [];
-
-    const url = 'https://disease.sh/v3/covid-19/countries/brazil,usa';
-
-    for (let i = 0; i < 2; i++) {
-      const data = this.httpService.get(url).pipe(
-        map((res) => res.data[i]),
-        map(
-          (data): Resultado => ({
-            país: data.country,
-            casosHoje: data.todayCases,
-            mortesHoje: data.todayDeaths,
-            data: new Date(),
-            ativos: data.active,
-            estadoCritico: data.critical,
-          }),
-        ),
-      );
-
-      const observable = await lastValueFrom(data);
-      result.push(observable);
-    }
-
-    return result;
+  getUsaAndBrazil() {
+    return this.getData('usa,brazil');
   }
 
-  async getChinaAndRussia() {
+  getChinaAndRussia() {
+    return this.getData('china,russia');
+  }
+
+  parseData() {}
+
+
+  async getData(c: string) {
     const result: Resultado[] = [];
 
-    const url = 'https://disease.sh/v3/covid-19/countries/china,russia';
+    const url = 'https://disease.sh/v3/covid-19/countries/' + c;
 
     for (let i = 0; i < 2; i++) {
       const data = this.httpService.get(url).pipe(
