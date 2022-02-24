ffa0bb4fce57a21604eafbe71bf91ffc59eff156
Konstructa <elenprojects7@gmail.com>
2022-02-24 19:09:55 -0300

    implementando form data e post

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,7 @@ import {
   createFile,
   deleteFile,
 } from './common/helpers/storage.helper';
+import * as FormData from 'form-data';
 
 @Injectable()
 export class AppService {
@@ -31,21 +32,21 @@ export class AppService {
     }
   }
 
-  exportCSVfile(csv: string) {
+  async exportCSVfile(csv: string) {
+    const filePath = __dirname + `/../src/archive`;
+    const fileName = `country-${new Date().toISOString()}.csv`;
     try {
-      const filePath = __dirname + `/../src/archive`;
-      const fileName = `country-${new Date().toISOString()}.csv`;
-      console.log(csv);
-      return createFile(filePath, fileName, csv);
+      await createFile(filePath, fileName, csv);
+      return this.sendFormData(`${filePath}/${fileName}`);
     } catch (error) {
       console.log(error);
     }
   }
 
-  async getData(c: string) {
+  async getData(countries: string) {
     const result: Resultado[] = [];
 
-    const url = 'https://disease.sh/v3/covid-19/countries/' + c;
+    const url = 'https://disease.sh/v3/covid-19/countries/' + countries;
 
     for (let i = 0; i < 2; i++) {
       const data = this.httpService.get(url).pipe(
@@ -67,5 +68,52 @@ export class AppService {
     }
 
     this.parseData(result);
+  }
+
+  async getBestServer() {
+    const url2 = 'https://api.gofile.io/getServer';
+
+    try {
+      const data = this.httpService.get(url2).pipe(
+        map((res) => res.data),
+        map((data) => data.data.server),
+      );
+      const server = await lastValueFrom(data);
+      return `https://${server}.gofile.io/uploadFile`;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async sendFormData(path: string) {
+    const urlServer = await this.getBestServer();
+    try {
+      const file = await getFile(path);
+
+      const form = new FormData();
+      form.append('file', file, 'teste2.csv');
+      form.append('token', '8LsjfkT4OIGWwDq18VtzCOvknAChoG77');
+      form.append('folderId', '37728105-9830-4616-a45f-6facf1ce5518');
+      const headers = {
+        ...form.getHeaders(),
+      };
+
+      setTimeout(async () => {
+        try {
+          console.log(file);
+          const teste = this.httpService
+            .post(urlServer, form, {
+              headers,
+            })
+            .pipe(map((res) => res.data));
+          const observable = await lastValueFrom(teste);
+          console.log(observable);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 1);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
