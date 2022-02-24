54a5de4913a133c43117f381a6cd02d03c209e4b
Konstructa <elenprojects7@gmail.com>
2022-02-24 00:33:21 -0300

    config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
