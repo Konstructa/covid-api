c6b534456f074c6d0dee20745ad15ecc70911c49
Konstructa <elenprojects7@gmail.com>
2022-02-24 21:56:52 -0300

    add cron

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
+@Injectable()
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
-  @Get('/usa-bra')
+  @Cron('0 21 */1 * *')
   getUsaAndBrazi() {
     return this.appService.getUsaAndBrazil();
   }
 
-  @Get('/china-russia')
+  @Cron('0 21 */1 * *')
   getChinaAndRussia() {
     return this.appService.getChinaAndRussia();
   }
